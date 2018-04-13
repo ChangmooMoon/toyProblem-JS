766402f4fb63e676012107d469132dd123956d33
moo25 <ansckdan@gmail.com>
2018-04-14 03:06:50 +0900

    8.largestProductOfThree sort ver

--- CS-50/08-largestProductOfThree/largestProductOfThree.js
+++ CS-50/08-largestProductOfThree/largestProductOfThree.js
@@ -1,3 +1,4 @@
+
 /* Write a function that finds the largest possible product of any three numbers
  * from an array.
  *
@@ -6,51 +7,18 @@
  *
  * Extra credit: Make your function handle negative numbers.
  */
-// 큰 숫자 3개의 element들의 곱,negative number도 포함
 
-// // 나이 최초 답안
-// var largestProductOfThree = function(array) {
-//   array.sort(function(a,b){return b-a});
-//   return array[0]*array[1]*array[2];
-// }
-// // =================================================================
-// // 시도2
-// var largestProductOfThree2 = function(array){
-//   var positive = array.forEach(function(x){
-//     if(x>=0) return x;
-//   })
-//   return positive;
-// }
-// console.log( largestProductOfThree2([-8,-3,1,2,5]) );
-// // [5,-8,2,-3,1]
 
-// soultuion 1
-var largestProductOfThree = function(array){
-  var temp = array.sort(function(a,b){
-    return a-b;
-  })
-  var positive = array[array.length-1]
-  *array[array.length-2]
-  *array[array.length-3]);
+const largestProductOfThree = (array) => {
+  if (array.length < 3) throw new Error('At least need more than 3 arguments')
 
-  var negative = array[0]*array[1]*array[array.length-2];
-  return positive > negative ? positive : negative;
+  array = array.sort((a, b) => { return a - b })
+  let length = array.length
+
+  const plusplusplus = array[length - 1] * array[length - 2] * array[length - 3] // case 1 : (+ + +)
+  const minusplusplus = array[0] * array[1] * array[length - 1] // case 2 : (- - +)
+
+  return plusplusplus >= minusplusplus ? plusplusplus : minusplusplus
 }
 
-//solution 2
-Array.prototype.sortAscending = function(){
-  this.sort(function(a,b){
-    return a-b;
-  });
-  return this;
-}
-
-var largestProductOfTHree = function(array){
-  array = array.slice().sortAscending();
-
-  var secondFromLast = array[array.length-2];
-  var thirdFromLast = array[array.length-3];
-
-  return Math.max(array[array.length-1]* secondFromLast*thirdFromLast,
-  array[array.length-1]* array.[0]* array[1]);
-};
+console.log(largestProductOfThree([-10, -4, 0, 2, 10, 11])) // 440 > 220
