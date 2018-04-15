11ff7c805585579f271f955fa5bf1281865d7252
moo25 <ansckdan@gmail.com>
2018-04-15 20:59:27 +0900

    15. using iterator

--- CS-50/15-binarySearchArray/binarySearchArray.js
+++ CS-50/15-binarySearchArray/binarySearchArray.js
@@ -7,15 +7,18 @@
  * var index = binarySearch([1, 2, 3, 4, 5], 4);
  * console.log(index); // [3]
  */
- function binarySearch(array,target,start,end){
-   if(start>end) return false
+const binarySearch = (array, target) => {
+  let
+    start = 0,
+    end = array.length - 1
 
-   var mid = Math.floor( (start+end) / 2)
+  while (start <= end) {
+    mid = Math.floor((start + end) / 2)
+    if (array[mid] === target) return [mid]
+    else if (array[mid] > target) end = mid - 1
+    else if (array[mid] < target) start = mid + 1
+  }
+}
 
-   if(array[mid] > target) return binarySearch(array,target,start,mid-1)  // (start,mid-1)
-   else if(array[mid] < target) return binarySearch(array,target,mid+1,end) // (mid+1,end)
-   else return mid
- }
-
-var index = binarySearch([1, 2, 3, 4, 5],6,0,4);
-console.log(index); // [3]
+var index = binarySearch([1, 3, 5, 7, 9, 11, 13, 15], 11);
+console.log(index); // [5]
