d9abd149ad6adadf7540d561626cf0e47757043b
moo25 <ansckdan@gmail.com>
2018-04-16 03:18:43 +0900

    19.string 처리문제

--- CS-50/19-longestRun/longestRun.js
+++ CS-50/19-longestRun/longestRun.js
@@ -4,47 +4,50 @@
  * that run. If there are two runs of equal length, return the first one.
  * For example:
  *
- *   longestRun("abbbcc") // [1, 3]
+ *   longestRun("abbbcc") // [1, 3] 0 1 2 3 4 5
  *   longestRun("aabbc")  // [0, 1]
- *   longestRun("abcd")   // [0, 0]
+ *   longestRun("abcd")   // [0, 0] 0 1 2 3
  *
  * Try your function with long, random strings to make sure it handles large
  * inputs well.
  */
- var longestRun = function(string) {
-   let arr = [1];
-   for (let i = 1; i < string.length; i++) {
-     if (string[i] === string[i - 1]) arr.push(arr[i - 1] + 1);
-     else arr.push(1);
-   }
-   let max = arr.slice().sort((a, b) => b - a)[0];
-   return [arr.indexOf(max) - max + 1, arr.indexOf(max)];
- };
 
+const longestRun = (string) => {
+  string = string.split('').sort()
+  console.log(string)
 
- var longestRun2 = function(string){ /// 0a 1b 2b 3b 4a
-   let arr = [1];
-   for(let i =1; i<string.length; i++){
-     if(string[i] === string[i-1])  arr.push(arr[i-1] +1); // string[1] === string[0]이면 string[0]+1을 푸쉬
-     else arr.push(1); // 아니면 1을 푸쉬 // 1.b랑 a는 다름. 그래서 [1,1],[1,1,2],[1,1,2,3],[1,1,2,3,1]
-   }
-   console.log(arr);
-   let max = arr.slice().sort((a,b) => b -a)[0]; //[3,2,1,1,1]
-   console.log(max);
-   return [arr.indexOf(max) - max +1, arr.indexOf(max)];
-   // [0-3+1,]
- };
+  let count = 1, start = 0, bestCount = 0, bestStart = 0, bestEnd = 0, i
+  for (i = 1; i < string.length; i++) {
+    if (string[i] === string[i - 1]) {
+      count++
+      if (count > bestCount) {
+        bestCount = count
+        bestStart = start
+        bestEnd = i
+      }
+    } else {
+      count = 1
+      start = i
+    }
+  }
+  return [bestStart, bestEnd];
+}
+//console.log(longestRun("abbbcc")) // [1, 3] 0 1 2 3 4 5
 
- // If you need a random string generator, use this!
- // (you wont need this function for your solution but it may help with testing)
- var randomString = function(len) {
-   var text = "";
-   var possible = "abcdefghijklmnopqrstuvwxyz";
+// If you need a random string generator, use this!
+// (you wont need this function for your solution but it may help with testing)
+var randomString = function (len) {
+  var text = "";
+  var possible = "abcdefghijklmnopqrstuvwxyz";
 
-   for (var i = 0; i < len; i++) {
-     text += possible.charAt(Math.floor(Math.random() * possible.length));
-   }
+  for (var i = 0; i < len; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
 
-   return text;
- };
-console.log(longestRun2('abbbcc'))
+  return text;
+}
+
+console.log(longestRun("abbbcc")) // [1, 3] 0 1 2 3 4 5
+console.log(longestRun("aabbc"))  // [0, 1]
+console.log(longestRun("abcd"))   // [0, 0] 0 1 2 3
+console.log(longestRun(randomString(10)))
