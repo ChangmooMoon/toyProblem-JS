08969dceff04ff83b3417ee34bf0c84cedc9db65
moo25 <ansckdan@gmail.com>
2018-04-18 19:04:56 +0900

    24. using roman number traits

--- CS-50/24-romanNumeralTranslator/romanNumeralTranslator.js
+++ CS-50/24-romanNumeralTranslator/romanNumeralTranslator.js
@@ -1,19 +1,21 @@
+
 /**
- * Given a roman numeral as input, write a function that converts the roman
- * numeral to a number and outputs it.
+ * Given a roman numeral as input, write a function that converts the roman numeral to a number and outputs it.
  *
  * Ex:
- * translateRomanNumeral("LX") // 60
+ * translateRomanNumeral("LX") // 60 = 50 + 10
  *
- * When a smaller numeral appears before a larger one, it becomes
- * a subtractive operation. You can assume only one smaller numeral
- * may appear in front of larger one.
+ * When a smaller numeral appears before a larger one,
+ * it becomes a subtractive operation. You can assume only one smaller numeral may appear in front of larger one.
  *
  * Ex:
- * translateRomanNumeral("IV") // 4
+ * translateRomanNumeral("IV") // 4 = -1 + 5
  *
  * You should return `null` on invalid input.
  */
+// I, II, III, IV, V, VI, VII, VIII, IX, X
+
+// XX === 20, XL === 40, XLIV === 44, LV === 55
 
 var DIGIT_VALUES = {
   I: 1,
@@ -24,19 +26,21 @@ var DIGIT_VALUES = {
   D: 500,
   M: 1000
 };
-
-// 1. IV 4 ,VI 6 2.LX 60
-
-var translateRomanNumeral = function(romanNumeral) {
-  var result = 0;
-  var a = romanNumeral.split('');
-  for (var i = 0; i < a.length; i++) { // 예외처리
-    if( DIGIT_VALUES[a[i]] === 5 || 50 || 500){
-      console.log(DIGIT_VALUES[a[i]])
-    }
-    result += DIGIT_VALUES[a[i]]
+const translateRomanNumeral = (romanNumeral) => {
+  let result = 0
+  for (let i = 0; i < romanNumeral.length; i += 2) {
+    let
+      a = DIGIT_VALUES[romanNumeral[i]],
+      b = DIGIT_VALUES[romanNumeral[i + 1]]
+    if (b === undefined) result += a
+    else if (a < b) result += (b - a)
+    else result += (a + b)
   }
-  return result;
+  return resultg
 }
 
-console.log(translateRomanNumeral('IV')); // 4
+console.log(translateRomanNumeral("LX")) // 60
+console.log(translateRomanNumeral("IV")) // 4
+console.log(translateRomanNumeral('XX')) // 20
+console.log(translateRomanNumeral('XL')) // 40
+console.log(translateRomanNumeral('XLIV')) // 44
