850e0a603f4a7664bfcbe92a55d70be13e30590d
moo25 <ansckdan@gmail.com>
2018-04-18 20:29:28 +0900

    24. resize function added

--- CS-50/25-hashTable/hashTable.js
+++ CS-50/25-hashTable/hashTable.js
@@ -3,44 +3,94 @@
  * The hashtable does not need to resize but it should still handle collisions.
  */
 
-var makeHashTable = function(){
-  var result = {};
-  var storage = [];
-  var storageLimit = 1000;
+const makeHashTable = () => {
+  let
+    result = {},
+    storage = [],
+    storageLimit = 1000
+  size = 0
 
-  result.insert = function(key,value){
-    
-    var index = getIndexBelowMaxForKey(key,storageLimit);
-    storage[index] = value;
-  };
+  result.insert = (key, value) => {
+    size++
+    let
+      index = getIndexBelowMaxForKey(key, storageLimit),
+      bucket = storage[index]
 
-  result.retrieve = function(key){
-    var index = getIndexBelowMaxForKey(key,storageLimit);
-    console.log(storage[index]);
-    return storage[index];
-  };
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) bucket[i][1] = value
+      }
+    } else {
+      storage[index] = []
+      storage[index].push([key, value])
+    }
+    if (size / storageLimit >= 0.75) result.resize(storageLimit * 2)
+  }
 
-  result.remove = function(/*...*/ ){
-  };
+  result.retrieve = (key) => {
+    let
+      index = getIndexBelowMaxForKey(key, storageLimit),
+      bucket = storage[index]
 
-  return result;
-};
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) return bucket[i][1]
+      }
+    } else return undefined
+  }
+
+  result.remove = (key) => {
+    let
+      index = getIndexBelowMaxForKey(key, storageLimit),
+      bucket = storage[index]
+
+    if (bucket) {
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          size--
+          bucket.splice(i, 1)
+        }
+      }
+    }
+    if (size / storageLimit <= 0.25) result.resize(storageLimit / 2)
+  }
+
+  result.resize = (newLimit) => {
+    let
+      oldStorage = storage,
+      storageLimit = newLimit,
+      storage = []
+
+    for (let i = 0; i < oldStorage.length; i++) {
+      let bucket = oldStorage[i]
+      if (bucket) {
+        for (let j = 0; j < bucket.length; j++) {
+          let
+            index = getIndexBelowMaxForKey(bucket[j][0], storageLimit),
+            newBucket = storage[index]
+          if (newBucket) newBucket.push([bucket[j][0], bucket[j][1]])
+          else {
+            newBucket = []
+            newBucket.push([bucket[j][0], bucket[j][1]])
+          }
+        }
+      }
+    }
+  }
+  return result
+}
 
 // This is a "hashing function". You don't need to worry about it, just use it
 // to turn any string into an integer that is well-distributed between
 // 0 and max - 1
-var getIndexBelowMaxForKey = function(str, max){
-  var hash = 0;
+const getIndexBelowMaxForKey = (str, max) => {
+  let hash = 0
   for (var i = 0; i < str.length; i++) {
-    hash = (hash<<5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
+    hash = (hash << 5) + hash + str.charCodeAt(i)
+    hash = hash & hash // Convert to 32bit integer
+    hash = Math.abs(hash)
   }
-  return hash % max;
-};
+  return hash % max
+}
 
-
-var hashtable = new makeHashTable();
-hashtable.insert(1,'hi');
-hashtable.insert(1,'hey');
-hashtable.retrieve(1);
+console.log(getIndexBelowMaxForKey('hi', 1024))
