d91772ec4c172aeda5dcb3aa81d0ad47e3486eb8
moo25 <ansckdan@gmail.com>
2018-04-18 20:59:03 +0900

    25. using es5 pattern

--- CS-50/26-linkedList/linkedList.js
+++ CS-50/26-linkedList/linkedList.js
@@ -16,24 +16,58 @@
 // list.removeHead(); //yields '4'
 // list.tail.value;   //yields '5';
 
-
-var LinkedList = function(){
-  //fill me in!
-};
-
+let LinkedList = function () {
+  this.head = null
+  this.tail = null
+}
 //write methods here!
+LinkedList.prototype.addToTail = function (val) {
+  let newNode = this.makeNode(val)
 
-LinkedList.prototype.addToTail = function(
-){
+  if (!this.head && !this.tail) {
+    this.head = newNode
+    this.tail = newNode
+  } else {
+    this.tail.next = newNode
+    this.tail = newNode
+  }
+}
+
+LinkedList.prototype.removeHead = function () {
+  if (!this.head) return null
+  else {
+    let removed = this.head.value
+    this.head = this.head.next
+    return removed
+  }
+}
+
+LinkedList.prototype.contains = function (val) {
+  if (!this.head) return false
+
+  let currentNode = this.head
+  while (currentNode) {
+    if (currentNode.value === val || this.tail.value === val) return true;
+    else currentNode = currentNode.next
+  }
+  return false;
+}
+
+LinkedList.prototype.makeNode = function (val) {
+  let node = {}
+
+  node.value = val
+  node.next = null
+
+  return node
 };
 
-LinkedList.prototype.removeHead = function(){
-};
-
-LinkedList.prototype.contains = function(
-){
-};
-
-LinkedList.prototype.makeNode = function(
-){
-};
+var list = new LinkedList()
+console.log(list.tail)   //yields 'null'
+list.addToTail(4)
+list.addToTail(5)
+console.log(list.head.value)   //yields '4';
+console.log(list.contains(5))  //yields 'true';
+console.log(list.contains(6))  //yields 'false';
+console.log(list.removeHead()) //yields '4'
+console.log(list.tail.value)   //yields '5';
