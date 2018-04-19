9fe9fa7ba027645139a04c604a6da209bb53a0bc
moo25 <ansckdan@gmail.com>
2018-04-19 22:39:28 +0900

    29. ref: https://www.youtube.com/watch\?v\=9QduzzW10uA

--- CS-50/29-robotPaths/robotPaths.js
+++ CS-50/29-robotPaths/robotPaths.js
@@ -1,33 +1,51 @@
 /**
- *  
- *  A robot located at the top left corner of a 5x5 grid is trying to reach the 
- *  bottom right corner. The robot can move either up, down, left, or right, 
- *  but cannot visit the same spot twice. How many possible unique paths are 
- *  there to the bottom right corner? 
- *  
+ *
+ *  A robot located at the top left corner of a 5x5 grid is trying to reach the
+ *  bottom right corner. The robot can move either up, down, left, or right,
+ *  but cannot visit the same spot twice. How many possible unique paths are
+ *  there to the bottom right corner?
+ *
  *  make your solution work for a grid of any size.
  *
  */
 
 // A Board class will be useful
 
-var makeBoard = function(n) {
-  var board = [];
-  for (var i = 0; i < n; i++) {
+const makeBoard = (n) => {
+  let board = []
+  for (let i = 0; i < n; i++) {
     board.push([]);
-    for (var j = 0; j < n; j++) {
+    for (let j = 0; j < n; j++) {
       board[i].push(false);
     }
   }
-  board.togglePiece = function(i, j) {
-    this[i][j] = !this[i][j];
-  }
-  board.hasBeenVisited = function(i, j) {
-    return !!this[i][j];
-  }
-  return board;
-};
-
-var robotPaths = function(n, board, i, j) {
+  board.togglePiece = function (i, j) { this[i][j] = !this[i][j] }
+  board.hasBeenVisited = function (i, j) { return !!this[i][j] }
+  return board
 }
 
+const robotPaths = (n) => {
+  let
+    count = 0,
+    board = makeBoard(n)
+
+  const counter = (x, y) => {
+    if (x === n - 1 && y === n - 1) { count++; return; }
+    else if (x < 0 || y < 0 || x >= n || y >= n) return
+    else if (board.hasBeenVisited(x, y)) return
+    else {
+      board.togglePiece(x, y)
+      counter(x - 1, y) // right
+      counter(x + 1, y) // left
+      counter(x, y - 1) // up
+      counter(x, y + 1) // down
+      board.togglePiece(x, y)
+    }
+  }
+
+  counter(0, 0)
+  return count
+}
+
+let a = robotPaths(5)
+console.log(a)
