3dd93258ba94a5655b7d1322187148bc386f507d
moo25 <ansckdan@gmail.com>
2018-04-20 21:19:10 +0900

    32.sieve of eratosthenes algorithm

--- CS-50/32-primeTester/primeTester.js
+++ CS-50/32-primeTester/primeTester.js
@@ -4,21 +4,29 @@
  * a prime number, false if it's not.
  */
 
-var primeTester = function(n) {
-  if(typeof n !== 'number' || n < 1 || n % 1 !== 0){
-    // n isn't a number or n is less than 1 or n is not an integer
-    return false;
-  }
-  // TODO: return true if n is prime, false otherwise
-};
+const primeTester = (n) => {
+  if (typeof n !== 'number' || n < 1 || n % 1 !== 0) return false
 
-/* Extra credit: Write a function that generates a list of all prime numbers
- * in a user-specified range (inclusive). If you're not quite sure where to start,
- * check out the Sieve of Eratosthenes on Wikipedia. (And if you're feeling
- * saucy, check out the Sieve of Atkin.)
+  if (n === 1) return false
+
+  for (let i = 2; i * i <= n; i++) {
+    if (n % i === 0) return false //
+  }
+  return true
+}
+
+/* Extra credit:
+Write a function that generates a list of all prime numbers in a user-specified range (inclusive).
+If you're not quite sure where to start, check out the Sieve of Eratosthenes on Wikipedia.
+(And if you're feeling saucy, check out the Sieve of Atkin.)
  */
 
-var primeSieve = function (start, end) {
-};
+const primeSieve = (start, end) => {
+  let result = []
+  for (let i = start; i < end; i++) {
+    if (primeTester(i) === true) result.push(i)
+  }
+  return result
+}
 
-
+console.log(primeSieve(1, 100))
