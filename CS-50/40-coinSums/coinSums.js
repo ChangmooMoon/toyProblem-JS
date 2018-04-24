610d3fb86f6bf51f6360514f2eab025bb87ac907
moo25 <ansckdan@gmail.com>
2018-04-25 02:16:35 +0900

    40. pipeline

--- CS-50/40-coinSums/coinSums.js
+++ CS-50/40-coinSums/coinSums.js
@@ -14,6 +14,7 @@ In England the currency is made up of pound, £, and pence, p, and there are eig
 It is possible to make £2 in the following way:
 
 1 * £1 + 1 * 50p + 2 * 20p + 1 * 5p + 1 * 2p + 3 * 1p
+// = 100p + 50p + 40p + 5p + 2p + 3p = 200p
 How many different ways can £2 be made using any number of coins?
 
 example usage of `makeChange`:
@@ -24,8 +25,26 @@ makeChange(1) === 1
 makeChange(2) === 2
 */
 
-var makeChange = function(total){
+const makeChange = (total) => {
+  let result = 0
+  const coins = [1, 2, 5, 10, 20, 50, 100, 200]
+  const changeCount = (index, total) => {
+    let current = coins[index]
+    if (index === 0) {
+      result++
+      return
+    }
+    while (total >= 0) {
+      changeCount(index - 1, total)
+      total -= current
+    }
+  }
+  changeCount(coins.length - 1, total) // 7,total
+  return result
+}
 
-};
-
-
+console.log(makeChange(1)) // 1
+console.log(makeChange(2)) // 11/2 // 2
+console.log(makeChange(5)) // 11111,2111,221,5 // 4
+console.log(makeChange(8))
+console.log(makeChange(200)) // 73682
